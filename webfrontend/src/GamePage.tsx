772bbd34108cee56d501d48ce5ba4bd07f2df2b3
I Guest <i.guest2010@gmail.com>
2024-04-02 20:24:24 +0100

    complete rough outline of game page

--- webfrontend/src/GamePage.tsx
+++ webfrontend/src/GamePage.tsx
@@ -33,13 +33,43 @@ export const GamePage = () => {
             text-align: center;
 
             & > *:not(:first-child) {
-              margin-top: 10px; /* Adds top margin to all elements except the first one */
+              margin-top: 20px; /* Adds top margin to all elements except the first one */
             }
           `}
         >
           <Title>Letters Round</Title>
-          <button onClick={onStartRunning}>Start Clock</button>
+          {/* <button
+            css={css`
+              visibility: collapse;
+            `}
+            onClick={onStartRunning}
+          >
+            Start Clock
+          </button> */}
           <Clock isRunning={isRunning} onComplete={handleOnComplete} />
+          <p>Vowel(v)/Consonant(c)?</p>
+          <input
+            css={css`
+              padding: 5px;
+            `}
+            type="text"
+            placeholder="Enter responses here..."
+            name="name"
+          ></input>
+          <div
+            css={css`
+              display: flex;
+              justify-content: center; /* Center the buttons horizontally */
+              gap: 20px; /* Adds some space between the two buttons */
+            `}
+          >
+            <button style={{ padding: '5px' }} disabled={true}>
+              Next Round
+            </button>
+            <button style={{ padding: '5px' }} disabled={true}>
+              Restart Game
+            </button>
+          </div>
         </div>
       </Page>
     </div>
