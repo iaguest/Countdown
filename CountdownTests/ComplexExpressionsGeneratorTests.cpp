d90c5cdc1b09535fa71dfe40de54d5274cd6db5a
I Guest <i.guest2010@gmail.com>
2024-04-27 12:52:48 +0100

    more test updates

--- CountdownTests/ComplexExpressionsGeneratorTests.cpp
+++ CountdownTests/ComplexExpressionsGeneratorTests.cpp
@@ -13,7 +13,7 @@ namespace ComplexExpGenTest
     static const std::vector<std::string> simpleExpressions{ "1+2", "1+2+3" };
     static const std::vector<std::string> threeNumExpression{ "1*2*3" };
     static const std::vector<std::string> threeNumExpressionResults = std::vector<std::string>{
-        "(1*2*3)", "(1*2)*3", "1*(2*3)", "((1*2*3))", "((1*2))*3", "1*((2*3))", "((1*2)*3)", "(1*(2*3))",
+        "(1*2*3)", "(1*2)*3", "1*(2*3)",
     };
     static const std::vector<std::string> fourNumExpression{ "1+2+3+4"};
     static const std::vector<std::string> fourNumExpressionResults = std::vector<std::string>{
@@ -35,6 +35,15 @@ TEST_CASE("Validate complex expressions generated for single three number expres
 
     REQUIRE(ComplexExpGenTest::threeNumExpressionResults.size() == output.size());
     REQUIRE(TestUtils::containsSubset(output, ComplexExpGenTest::threeNumExpressionResults));
+}
+
+TEST_CASE("Validate complex expressions contain no duplicates") {
+    auto gen = ComplexExpressionsGenerator(ComplexExpGenTest::fourNumExpression);
+
+    const auto& output = TestUtils::toResultVector(gen);
+
+    const std::set<std::string> noDuplicates(output.begin(), output.end());
+    REQUIRE(TestUtils::containsSubset(std::vector(noDuplicates.begin(), noDuplicates.end()), output));
 }
 
 TEST_CASE("Validate complex expressions generated for single four number expression") {
@@ -59,15 +68,6 @@ TEST_CASE("Validate complex expressions generated for multiple input expressions
         ComplexExpGenTest::fourNumExpressionResults.size()) == output.size());
     REQUIRE(TestUtils::containsSubset(output, ComplexExpGenTest::threeNumExpressionResults));
     REQUIRE(TestUtils::containsSubset(output, ComplexExpGenTest::fourNumExpressionResults));
-}
-
-TEST_CASE("Validate complex expressions contain no duplicates") {
-    auto gen = ComplexExpressionsGenerator(ComplexExpGenTest::fourNumExpression);
-
-    const auto& output = TestUtils::toResultVector(gen);
-
-    const std::set<std::string> noDuplicates(output.begin(), output.end());
-    REQUIRE(TestUtils::containsSubset(std::vector(noDuplicates.begin(), noDuplicates.end()), output));
 }
 
 TEST_CASE("Complex expressions generator handles five number expression")
