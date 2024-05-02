0cd0cf1fd776c1139316d02d62c200f994fb3165
I Guest <i.guest2010@gmail.com>
2024-05-02 08:39:41 +0100

    another unit test

--- CountdownTests/ParenthesizedExpressionsGeneratorTests.cpp
+++ CountdownTests/ParenthesizedExpressionsGeneratorTests.cpp
@@ -9,18 +9,15 @@
 
 namespace ParenExpGenTest
 {
-
 static const std::string threeNumExpression{ "1+2+3" };
-
 static const std::string fourNumExpression{ "1+2+3+4" };
-
 }
 
 TEST_CASE("ParenthesizedExpressionsGenerator is constructable") {
     REQUIRE_NOTHROW(ParenthesizedExpressionsGenerator(ParenExpGenTest::threeNumExpression, 1));
 }
 
-TEST_CASE("Validate parenthesized expressions generated in three number case") {
+TEST_CASE("Validate parenthesized expressions for three number case with one pair of parentheses") {
     const std::vector<std::string> expected = std::vector<std::string>{ "(1+2)+3", "(1+2+3)", "1+(2+3)" };
 
     auto gen = ParenthesizedExpressionsGenerator(ParenExpGenTest::threeNumExpression, 1);
@@ -30,11 +27,25 @@ TEST_CASE("Validate parenthesized expressions generated in three number case") {
     REQUIRE(TestUtils::containsSubset(output, expected));
 }
 
-TEST_CASE("Validate parenthesized expressions generated in four number case") {
+TEST_CASE("Validate parenthesized expressions for four number case with one pair of parentheses") {
     const std::vector<std::string> expected = std::vector<std::string>{
         "(1+2)+3+4", "(1+2+3)+4", "(1+2+3+4)", "1+(2+3)+4", "1+(2+3+4)", "1+2+(3+4)" };
 
     auto gen = ParenthesizedExpressionsGenerator(ParenExpGenTest::fourNumExpression, 1);
+    const auto& output = TestUtils::toResultVector(gen);
+
+    REQUIRE(expected.size() == output.size());
+    REQUIRE(TestUtils::containsSubset(output, expected));
+}
+
+TEST_CASE("Validate parenthesized expressions for four number case with two pairs of parentheses") {
+    const std::vector<std::string> expected = std::vector<std::string>{
+        "((1+2))+3+4", "((1+2)+3)+4", "((1+2)+3+4)", "((1+2+3))+4",
+        "((1+2+3)+4)", "((1+2+3+4))", "(1+(2+3))+4", "(1+(2+3)+4)",
+        "(1+(2+3+4))", "(1+2+(3+4))", "1+((2+3))+4", "1+((2+3)+4)",
+        "1+((2+3+4))", "1+(2+(3+4))", "1+2+((3+4))", "(1+2)+(3+4)"};
+
+    auto gen = ParenthesizedExpressionsGenerator(ParenExpGenTest::fourNumExpression, 2);
     const auto& output = TestUtils::toResultVector(gen);
 
     REQUIRE(expected.size() == output.size());
